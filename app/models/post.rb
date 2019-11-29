4caa75abe0dd36637c356e52737f1858c60ec36b
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-11-28 22:33:20 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Nonfiction"]}
+
+    validate :non_clickbait?
+
+    def non_clickbait?
+        
+        if title
+            if !(title.match(/Won't Believe/i)) and !(title.match(/Secret/i)) and !(title.match(/Top[number]/i)) and !(title.match(/Guess/i))
+                errors.add(:base, "Title must contains Won't Believe, Secret, Top[number], or Guess")
+            end
+        end
+    end
+    
 end
